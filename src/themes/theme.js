b109a140c050a82a9bd84838f7add0987b3b4fc0
Mikołaj <miki70707@interia.pl>
2020-05-18 12:02:21 +0200

    K4 theme.js add

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,3 +1,19 @@
 export default {
-
+    colors: {
+        primary: "#FF9900",
+        white: "#FFFFFF",
+        secondary: "#31302D",
+        text: "#31302D",
+        background: "#EFEFEF",
+    },
+    fonts: {
+        xl: "7rem",
+        l: "3.5rem",
+        m: "2.6rem",
+        s: "2.3rem",
+        xs: "2rem",
+        bold: 700,
+        medium: 500,
+        thin: 400,
+    }
 }
