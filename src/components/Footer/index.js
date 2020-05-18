148d9ac665c43b6f3e4f8da00640b26b3121c29f
Mikołaj <miki70707@interia.pl>
2020-05-18 12:13:48 +0200

    K7 File structure in components folder add

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return (<p>Stopka</p>);
+}
+
+export default Footer;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Nav = () => {
+    return (<p>Navigacja</p>);
+}
+
+export default Nav;
