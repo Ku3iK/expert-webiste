7b9dce5e20e4aeb6ec44aabbd80a312415d7bf3b
Mikołaj <miki70707@interia.pl>
2020-05-20 22:22:50 +0200

    C3 height of element redefined

--- src/components/H1/H1.js
+++ src/components/H1/H1.js
@@ -3,12 +3,12 @@ import styled from "styled-components"
 
 export default styled.h1`
   display: inline-block;
-  height: 40px;
+  height: 58px;
   width: auto;
   background-color: #fff;
   border-top: 4px solid #fcaa30;
   color: #31302d;
-  padding: 5px 25px;
+  padding: 5px 40px;
   font-size: 5rem;
   margin: 0 20px;
   ::first-letter {
