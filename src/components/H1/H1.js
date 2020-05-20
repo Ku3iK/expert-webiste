011269a48fd7aa61f94d8cc1770e64a04117a219
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-20 17:01:04 +0200

    C3 Add H1 component create

---
+++ src/components/H1/H1.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+export default styled.h1`
+  display: inline-block;
+  height: 40px;
+  width: auto;
+  background-color: #fff;
+  border-top: 4px solid #fcaa30;
+  color: #31302d;
+  padding: 5px 25px;
+  ::first-letter {
+    text-transform: uppercase;
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import H1 from "../components/H1/H1"
 
 const IndexPage = () => (
   <>
     <p>EXPERT WEBSITE</p>
+    <H1>rzuć okiem na nasze zdjęcia</H1>
   </>
 )
 
