5a72a423d419f455a4a3796e869462bcdef755ee
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-20 16:18:19 +0200

    C11 Informative element, creation and stylization

---
+++ src/components/InfoElement/InfoElement.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+export default styled.button`
+  height: 60px;
+  padding: 5px 15px;
+  font-size: 2.2rem;
+  background-color: #fff;
+  border-bottom: 10px solid #31302d;
+  color: #31302d;
+  ::first-letter {
+    text-transform: uppercase;
+  }
+`
