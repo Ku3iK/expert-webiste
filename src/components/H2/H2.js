b16cd7462dc67cbb55479aadde472dc2e1a5560e
Mikołaj <miki70707@interia.pl>
2020-05-25 18:43:48 +0200

    H2 component rwd add and trash styles delete

--- src/components/H2/H2.js
+++ src/components/H2/H2.js
@@ -2,11 +2,10 @@ import React from "react"
 import styled from "styled-components"
 
 export default styled.h2`
-  display: inline-block;
-  width: auto;
   color: ${props => props.color};
-  padding: 5px 40px;
-  font-size: 3.5rem;
-  margin: 0 20px;
+  font-size: ${({ theme }) => theme.font.l};
   text-transform: uppercase;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.font.m};
+  }
 `
