4459fd3f0f06fa33238e8ef1d92af488db71899d
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-25 14:22:00 +0200

    H2 mobile version create and change color and fonts from props to theme

--- src/components/H2/H2.js
+++ src/components/H2/H2.js
@@ -4,9 +4,16 @@ import styled from "styled-components"
 export default styled.h2`
   display: inline-block;
   width: auto;
-  color: ${props => props.color};
+  color: ${({ theme }) => theme.color.text};
   padding: 5px 40px;
-  font-size: 3.5rem;
-  margin: 0 20px;
+  font-size: ${({ theme }) => theme.font.l};
   text-transform: uppercase;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.font.m};
+    padding: 5px 30px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: ${({ theme }) => theme.font.xxs};
+    padding: 5px 10px;
+  }
 `
