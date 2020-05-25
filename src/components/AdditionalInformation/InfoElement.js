45820d6a06bab81e93d798b4d4b895344df7ae01
Mikołaj <miki70707@interia.pl>
2020-05-25 22:56:16 +0200

    Info Element move

--- src/components/AdditionalInformation/InfoElement.js
+++ src/components/AdditionalInformation/InfoElement.js
@@ -6,17 +6,18 @@ export default styled.div`
   justify-content: center;
   align-items: center;
   height: 60px;
-  font-size: ${({ theme }) => theme.font.m};
+  font-size: ${({ theme }) => theme.font.s};
   background-color: ${({ theme }) => theme.color.white};
-  border-bottom: 10px solid ${({ theme }) => theme.color.secondary};
+  border-bottom: 8px solid ${({ theme }) => theme.color.secondary};
   color: ${({ theme }) => theme.color.secondary};
   flex-grow: 2;
+  padding: 5px 15px;
   margin: 20px 20px 20px 0;
   ::first-letter {
     text-transform: uppercase;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    font-size: ${({ theme }) => theme.font.s};
+    font-size: 1.6rem;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: ${({ theme }) => theme.font.xxs};
