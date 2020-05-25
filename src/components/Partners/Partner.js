8d530a8eff5b3dbfdd8a5a43de09bdd455fc96fe
Mikołaj <miki70707@interia.pl>
2020-05-25 14:23:48 +0200

    C9 component Partner add

---
+++ src/components/Partners/Partner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Partner = ({ file, children }) => {
+    return (
+        <PartnerWarpper>
+            <img src={file} alt="dfsd" />
+            {children}
+        </PartnerWarpper>
+    );
+}
+
+export default Partner;
+
+const PartnerWarpper = styled.article`
+    max-width: 450px;
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 1;
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        max-width: 100%;
+    }
+    * {
+        margin: 1rem 0;
+    }
+    img {
+        height: 90px;
+        width: auto;
+        align-self: flex-start;
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+            height: auto;
+            width: 100%;
+        }
+    }
+    button {
+        align-self: flex-end;
+    }
+`;
+
