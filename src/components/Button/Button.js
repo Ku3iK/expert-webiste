85fbc52fbec5c58addcda0da22916b233b62a6b3
Mikołaj <miki70707@interia.pl>
2020-05-25 13:50:36 +0200

    Button compnents recomposition

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,11 @@ import React from "react"
 import styled from "styled-components"
 
 export default styled.button`
-  width: 146px;
-  height: 35px;
+  padding: .5rem 3rem;
   background-color: #fcaa30;
   border-bottom: 4px solid #030303;
+  font-size: ${({ theme }) => theme.font.m};
+  font-weight: ${({ theme }) => theme.font.bold};
   color: #fff;
   ::first-letter {
     text-transform: uppercase;
