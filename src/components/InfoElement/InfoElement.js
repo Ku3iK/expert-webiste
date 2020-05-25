016c9b4bcb28511a469201e0d5081d2e5f63bc5c
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-25 14:54:53 +0200

    Info element create mobile version and chagne color and fonts from props to theme

--- src/components/InfoElement/InfoElement.js
+++ src/components/InfoElement/InfoElement.js
@@ -1,16 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 
-export default styled.button`
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 60px;
-  padding: 5px 15px;
-  font-size: 2.2rem;
-  background-color: #fff;
-  border-bottom: 10px solid #31302d;
-  color: #31302d;
-  margin: 20px;
+  font-size: ${({ theme }) => theme.font.m};
+  background-color: ${({ theme }) => theme.color.white};
+  border-bottom: 10px solid ${({ theme }) => theme.color.secondary};
+  color: ${({ theme }) => theme.color.secondary};
   flex-grow: 2;
+  margin: 20px 20px 20px 0;
   ::first-letter {
     text-transform: uppercase;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.font.s};
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: ${({ theme }) => theme.font.xxs};
+    margin: 10px 0;
+    width: 100%;
   }
 `
