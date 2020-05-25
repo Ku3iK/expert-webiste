a97a770ef05e1fcceff3372cf4673a3abb329ced
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-25 14:34:07 +0200

    Button change color and fonts from props to theme

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,8 +4,8 @@ import styled from "styled-components"
 export default styled.button`
   width: 146px;
   height: 35px;
-  background-color: #fcaa30;
-  border-bottom: 4px solid #030303;
+  background-color: ${({ theme }) => theme.color.primary};
+  border-bottom: 4px solid ${({ theme }) => theme.color.secondary};
   color: #fff;
   ::first-letter {
     text-transform: uppercase;
