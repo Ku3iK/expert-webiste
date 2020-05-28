c775d1ede0575699a6532d0874129dff142d9262
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-05-28 23:42:20 +0200

    Contact page components add

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,75 @@
 import React from "react"
+import styled from "styled-components"
+import H1 from "../components/H1/H1"
+import Form from "../components/Form/Form"
+import P2 from "../components/P2/P2"
+import ContactInformation from "../components/ContactInformations/ContactInformation"
 
-const NotFoundPage = () => (
-    <p>Kontakt</p>
+const Contact = () => (
+  <ContactPage>
+    <H1>Skontaktuj się z nami</H1>
+    <div className="contactWrapper">
+      <div>
+        <P2>
+          Zapraszamy do kontaktu z nami poprzez formularz zgłoszeniowy lub
+          kontakt telefoniczny i mailowy.
+        </P2>
+        <div>
+          <ContactInformation title="telefon" content="534 534 542" />
+          <ContactInformation
+            title="mail"
+            content="expert.szczenic@gmial.com"
+          />
+          <ContactInformation
+            title="adres"
+            content="ul. blafsfsd 45 45-532 Szczecin"
+          />
+        </div>
+      </div>
+      <div>
+        <Form />
+      </div>
+    </div>
+  </ContactPage>
 )
 
-export default NotFoundPage
+export default Contact
+
+const ContactPage = styled.div`
+  margin: 10rem 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    margin: 6rem 0;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin: 2rem 0 6rem;
+  }
+  div.contactWrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin: 2rem 0 0 0;
+    div {
+      width: 100%;
+    }
+    > div:nth-of-type(1) {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      p:first-child {
+        padding: 3rem 0;
+        width: 80%;
+        font-size: ${({ theme }) => theme.font.m};
+      }
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+      display: flex;
+      flex-direction: column;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+      display: flex;
+      p:first-child {
+        font-size: ${({ theme }) => theme.font.s};
+      }
+    }
+  }
+`
