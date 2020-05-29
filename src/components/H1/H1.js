0280936ac809d7aafbbfbe5441e94fe0c8e70515
Mikołaj <miki70707@interia.pl>
2020-05-29 15:45:48 +0200

    H1 styles change

--- src/components/H1/H1.js
+++ src/components/H1/H1.js
@@ -3,23 +3,24 @@ import styled from "styled-components"
 
 export default styled.h1`
   display: inline-block;
-  height: 40px;
+  height: 57px;
   width: auto;
   background-color: ${({ theme }) => theme.color.white};
   border-top: 4px solid #fcaa30;
   color: ${({ theme }) => theme.color.text};
   padding: 5px 40px;
-  font-size: ${({ theme }) => theme.font.l};
+  font-size: ${({ theme }) => theme.font.xl};
   text-transform: uppercase;
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
     font-size: ${({ theme }) => theme.font.l};
-    padding: 5px 30px;
+    padding: 5px 20px;
     height: 40px;
     letter-spacing: 0px;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    font-size: ${({ theme }) => theme.font.m};
+    font-size: 3rem;
     padding: 5px 15px;
     height: auto;
+    width: 100%;
   }
 `
