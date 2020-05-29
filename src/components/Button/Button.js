f401e9f7f16ac661f1693be4176eeab53b8e6b7c
Mikołaj <miki70707@interia.pl>
2020-05-29 15:46:15 +0200

    Button desktop width change

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,6 +4,7 @@ export default styled.button`
   background-color: ${({ theme }) => theme.color.primary};
   border-bottom: 4px solid ${({ theme }) => theme.color.secondary};
   padding: 0.7rem 3rem;
+  min-width: 200px;
   font-size: ${({ theme }) => theme.font.m};
   font-weight: ${({ theme }) => theme.font.bold};
   color: #fff;
@@ -11,4 +12,7 @@ export default styled.button`
     text-transform: uppercase;
   }
   cursor: pointer;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    min-width: auto;
+  }
 `
