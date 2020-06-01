4d13d90811109499f3dca0945b8811d72110be68
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-06-01 11:46:50 +0200

    About page opinions add

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 import AboutUsElement from "../components/AboutUsElement/AboutUsElement"
-import img1 from "../assets/images/about1.svg"
-import img2 from "../assets/images/about2.svg"
 import H1 from "../components/H1/H1"
 
+import img1 from "../assets/images/about1.svg"
+import img2 from "../assets/images/about2.svg"
+
+import opinion1 from "../assets/images/opinion/opinia1.png"
+import opinion2 from "../assets/images/opinion/opinia2.png"
+import opinion3 from "../assets/images/opinion/opinia3.png"
+import opinion4 from "../assets/images/opinion/opinia4.png"
+import opinion5 from "../assets/images/opinion/opinia5.png"
+import opinion6 from "../assets/images/opinion/opinia6.png"
+import opinion7 from "../assets/images/opinion/opinia7.png"
+import Opinion from "../components/Opinion/Opinion"
+
 const NotFoundPage = () => {
-  const dataElements = [
+  const aboutData = [
     {
       img: img1,
       content:
@@ -18,19 +28,43 @@ const NotFoundPage = () => {
         "Firma Expert od lat zajmuje się montażami stolarni otworowej, posiadamy w swojej ofercie szeroką gamę produktów, które z pewnością spełnią Państwa wymagania.",
     },
   ]
+
+  const opinionData = [
+    {
+      img: opinion1,
+    },
+    {
+      img: opinion2,
+    },
+    {
+      img: opinion3,
+    },
+    {
+      img: opinion4,
+    },
+    {
+      img: opinion5,
+    },
+    {
+      img: opinion6,
+    },
+    {
+      img: opinion7,
+    },
+  ]
   return (
     <Container>
       <H1>Kim jesteśmy?</H1>
+      <AboutUsElement img={aboutData[0].img} content={aboutData[0].content} />
       <AboutUsElement
-        img={dataElements[0].img}
-        content={dataElements[0].content}
-      />
-      <AboutUsElement
-        img={dataElements[1].img}
-        content={dataElements[1].content}
+        img={aboutData[1].img}
+        content={aboutData[1].content}
         isFirst
       />
       <H1>Nasze opinie</H1>
+      <OpinionContainer>
+        <Opinion opinionData={opinionData} />
+      </OpinionContainer>
     </Container>
   )
 }
@@ -44,5 +78,31 @@ const Container = styled.div`
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     margin: 12rem 0 6rem;
+  }
+`
+
+const OpinionContainer = styled.div`
+  margin: 12rem 0 10rem;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    margin: 12rem 0 6rem;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin: 12rem 0 6rem;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+      flex-direction: column;
+      padding: 2rem 0;
+      width: 100%;
+      img {
+        width: 70%;
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+          width: 100%;
+        }
+      }
+    }
   }
 `
