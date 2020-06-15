2e87d3a169bde40b4b50c8bc5c4c5095139e8de9
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-06-15 23:25:53 +0200

    Dato cms elements for header add

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,8 +16,10 @@ const Layout = ({ children, location }) => {
       query {
         allDatoCmsContent {
           nodes {
-            headercontentde
             heaercontent
+            headercontentde
+            ouroffer
+            ourofferde
             footercontentde
             footercontent
           }
@@ -25,11 +27,20 @@ const Layout = ({ children, location }) => {
       }
     `
   )
-  const { headercontentde, heaercontent, footercontentde, footercontent } = allDatoCmsContent.nodes[0]
+  const {
+    headercontentde,
+    heaercontent,
+    ouroffer,
+    ourofferde,
+    footercontentde,
+    footercontent,
+  } = allDatoCmsContent.nodes[0]
   return (
     <ThemeProvider theme={theme}>
       <Helmet
-        htmlAttributes={{ "lang": /^\/de.*$/.test(location.pathname) ? "de" : "pl" }}
+        htmlAttributes={{
+          lang: /^\/de.*$/.test(location.pathname) ? "de" : "pl",
+        }}
       >
         <meta charSet="utf-8" />
         <title>EXPERT</title>
@@ -41,9 +52,23 @@ const Layout = ({ children, location }) => {
           name="keywords"
           content="Gorzów Wielkopolski, Gorzów, Drezdenko, Strzelce krajeńskie, Kłodawa, Barlinek, Lipiany, Myślibórz, Chojna, Mieszkowice, Dębno, Kostrzyn nad odrą, Krzeszyce, Deszczno, Bogdaniec, Witnica, Ośno lubuskie, Sulęcin, Międzyrzecz, Przytoczna, Tęczyce, Lubniewice, Pszczew, Słońsk, Świecko, Rzepin, Słubice, okna, drzwi, rolety, bramy garażowe, okna PCV, okna drewniane, okna aluminiowe, konigurator okien, drzwi pcv, monraż okien Gorzów Wielkopolski, montaż drzwi Gorzów Wielkopolski, montaż okien, drutex, Fart Produkt, montaż stolarki otworowej, stolarka otworowa"
         />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
@@ -54,15 +79,36 @@ const Layout = ({ children, location }) => {
         {/^\/de.*$/.test(location.pathname) ? (
           <Nav routes={routesDe} isPolish={true} location={location} />
         ) : (
-            <Nav routes={routes} location={location} />
-          )}
-        {location.pathname === "/" ? <Header>{heaercontent}</Header> : null}
-        {location.pathname === "/de/" ? <Header>{headercontentde}</Header> : null}
+          <Nav routes={routes} location={location} />
+        )}
+        {location.pathname === "/" ? (
+          <Header
+            headerText={heaercontent}
+            secondText={ouroffer}
+            buttonText={"Oferta"}
+          />
+        ) : null}
+        {location.pathname === "/de/" ? (
+          <Header
+            headerText={headercontentde}
+            secondText={ourofferde}
+            buttonText={"die Offerte"}
+          />
+        ) : null}
         <main>{children}</main>
-        {/^\/de.*$/.test(location.pathname)
-          ? <Footer routes={routesDe} text={footercontentde} authors={"Die Website wurde entworfen und erstellt von:"} />
-          : <Footer routes={routes} text={footercontent} authors={"Stronę zaprojektowali i wykonali"} />
-        }
+        {/^\/de.*$/.test(location.pathname) ? (
+          <Footer
+            routes={routesDe}
+            text={footercontentde}
+            authors={"Die Website wurde entworfen und erstellt von:"}
+          />
+        ) : (
+          <Footer
+            routes={routes}
+            text={footercontent}
+            authors={"Stronę zaprojektowali i wykonali"}
+          />
+        )}
       </div>
     </ThemeProvider>
   )
